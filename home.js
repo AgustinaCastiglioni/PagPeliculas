186829c7ba8289bafbc73a978032a988edcd8ec7
Agustina <Castiglioni>
2019-06-07 11:59:47 -0300

    Agregue carrousel de mejores puntuadas y proximamente disponibles.

--- home.js
+++ home.js
@@ -27,3 +27,61 @@ document.querySelector(".photos").innerHTML+= "<li> <img src= " + url + imagen +
 
   .catch(function(error) { console.log("Error: " + error);
   })}
+
+
+
+    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=063b16f0b4b52316bdf354da4c0177d7&language=en-US&page=1")
+    .then(function(response) { return response.json()
+    console.log(response);
+    })
+
+
+    .then(function(information) {
+
+  var arrayDePeliculas= information.results
+
+  console.log(information.results);
+
+    for (var i = 0; i < arrayDePeliculas.length; i++) {
+      var titulo= arrayDePeliculas[i].original_title
+      var imagen= arrayDePeliculas[i].poster_path
+      var id= arrayDePeliculas[i].id
+        var url = "https://image.tmdb.org/t/p/original"
+  document.querySelector(".MEJORESPUNTUADAS").innerHTML+= "<h2><a href= detalle.html?idGif="+ id + ">" +  titulo + "</a></h2>"
+  document.querySelector(".fotos").innerHTML+= "<li> <img src= " + url + imagen + "> </li>"
+  }
+
+
+
+    })
+
+    .catch(function(error) { console.log("Error: " + error);
+    })
+
+    fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=063b16f0b4b52316bdf354da4c0177d7&language=en-US&page=1")
+    .then(function(response) { return response.json()
+    console.log(response);
+    })
+
+
+    .then(function(information) {
+
+  var arrayDePeliculas= information.results
+
+  console.log(information.results);
+
+    for (var i = 0; i < arrayDePeliculas.length; i++) {
+      var titulo= arrayDePeliculas[i].original_title
+      var imagen= arrayDePeliculas[i].backdrop_path
+      var id= arrayDePeliculas[i].id
+        var url = "https://image.tmdb.org/t/p/original"
+  document.querySelector(".PROXIMAMENTE").innerHTML+= "<h2><a href= detalle.html?idGif="+ id + ">" +  titulo + "</a></h2>"
+  document.querySelector(".fotosprox").innerHTML+= "<li> <img src= " + url + imagen + "> </li>"
+  }
+
+
+
+    })
+
+    .catch(function(error) { console.log("Error: " + error);
+    })
